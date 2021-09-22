8c0347f3041b934cb4754795740e89ca8458483f
IgorPotapov15 <ingvarr.h@gmail.com>
2021-09-22 09:21:32 +0300

    changed hover color of button and made disabling filter buttons according to the filter state

--- src/components/FilterItems.js
+++ src/components/FilterItems.js
@@ -1,20 +1,21 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { changeFilter } from '../redux/todoSlice'
 
 const FilterItems = () => {
   const dispatch = useDispatch()
+  const filter = useSelector(state => state.todos.filter)
   
   const handleChange = (type) => {
     dispatch(
       changeFilter({ filter: type })
     )
   }
-
+  
   return (
     <div className="filter">
-      <button onClick={() => handleChange('ALL')} className="filter-button">All</button>
-      <button onClick={() => handleChange('DONE')} className="filter-button">Done</button>
-      <button onClick={() => handleChange('UNDONE')} className="filter-button">Undone</button>
+      <button disabled={filter === 'ALL' ? true : false} onClick={() => handleChange('ALL')} className="filter-button">All</button>
+      <button disabled={filter === 'DONE' ? true : false} onClick={() => handleChange('DONE')} className="filter-button">Done</button>
+      <button disabled={filter === 'UNDONE' ? true : false} onClick={() => handleChange('UNDONE')} className="filter-button">Undone</button>
     </div>
   )
 }
